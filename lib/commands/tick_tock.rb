d1721cf9a3ab291a99b27a57706f83ccb8e74814
Michael Ries <michael@riesd.com>
2013-11-07 21:19:00 -0700

    Turned the tick tock command into an actual multi-threaded command

--- lib/commands/tick_tock.rb
+++ lib/commands/tick_tock.rb
@@ -4,7 +4,33 @@ module Commands
     end
 
     def go
-      puts "OHAI"
+      clock = GrandfatherClock.new(10)
+      echoer = Echoer.new
+      clock.wind(echoer)
+      clock.wind(echoer, :tock)
+    end
+  end
+
+  class GrandfatherClock
+    include Celluloid
+
+    def initialize(times)
+      @times = times
+    end
+
+    def wind(output, msg = :tick)
+      @times.times do
+        output << msg
+        sleep 1
+      end
+    end
+  end
+
+  class Echoer
+    include Celluloid
+
+    def <<(msg)
+      p msg
     end
   end
 end
